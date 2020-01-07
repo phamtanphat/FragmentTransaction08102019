d131e79da731b843d5e2d1e1fa3d62876098a092
phatdroid94 <phatdroid94@gmail.com>
2020-01-07 21:20:02 +0700

    life cycle

--- app/src/main/java/com/example/fragmenttransaction08102019/AndroidFragment.java
+++ app/src/main/java/com/example/fragmenttransaction08102019/AndroidFragment.java
@@ -1,31 +1,72 @@
 package com.example.fragmenttransaction08102019;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AndroidFragment extends Fragment {
 
-
-    public AndroidFragment() {
-        // Required empty public constructor
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d("BBB","onAttach");
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("BBB","onCreate");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.d("BBB","onCreateView");
         return inflater.inflate(R.layout.fragment_android, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d("BBB","onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("BBB","onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("BBB","onResume");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("BBB","onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("BBB","onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("BBB","onDetach");
+    }
 }
