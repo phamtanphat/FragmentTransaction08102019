eb81503528c5e319f712a1494f02ffcffa82a091
phatdroid94 <phatdroid94@gmail.com>
2020-01-07 21:31:07 +0700

    transaction

--- app/src/main/java/com/example/fragmenttransaction08102019/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransaction08102019/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fragmentManager = getSupportFragmentManager();
+        Set<String> strings = new HashSet<>();
+        strings.add("A");
+        strings.add("B");
+        strings.add("C");
+        strings.add("D");
+        strings.add("D");
 
+        Iterator iterator = strings.iterator();
+        while (iterator.hasNext()){
+            Log.d("BBB",iterator.next().toString());
+        }
     }
 
     public void addAndroid(View view) {
@@ -31,5 +46,20 @@ public class MainActivity extends AppCompatActivity {
         IosFragment iosFragment = new IosFragment();
         fragmentTransaction.add(R.id.liearlayoutContainer,iosFragment);
         fragmentTransaction.commit();
+    }
+
+    public void replaceAndroid(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer,androidFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceIos(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer,iosFragment);
+        fragmentTransaction.commit();
+
     }
 }
